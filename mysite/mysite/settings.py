a0021ae8ef3036dc86e924f3c3485a309de67d95
kevinsheu <kevin.cp.sheu@gmail.com>
2013-07-29 12:03:19 -0700

    test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -115,8 +115,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/kevin/Dropbox/work/wordseer/wseer/mysite/mysite/registration/templates"
-    
+    "/home/kevin/Dropbox/work/wordseer/wseer/mysite/mysite/registration/templates",
+    "/home/kevin/Dropbox/work/wordseer/wseer/mysite/fileupload/templates",
 )
 
 INSTALLED_APPS = (
