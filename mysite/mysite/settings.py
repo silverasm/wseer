6488772feedfb2a0784000ced606179e713a7e38
PlasmaSheep <plasmasheep@gmail.com>
2013-08-27 16:07:11 -0700

    Fixed issues with css

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/Users/sudhasinha/wseer/mysite/mysite/fileupload/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -75,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/sudhasinha/wseer/mysite/mysite/fileupload/static',
+    '/srv/http/wseer/mysite/mysite/fileupload/static',
 )
 
 # List of finder classes that know how to find static files in
