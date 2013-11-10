ffb5aa625658a8eb031fcf4b10b531eafe5e939d
PlasmaSheep <plasmasheep@gmail.com>
2013-11-10 10:54:40 -0800

    Upgraded django-registration version

--- wseeruploader/apps/registration/backends/simple/urls.py
+++ wseeruploader/apps/registration/backends/simple/urls.py
@@ -24,13 +24,11 @@ from django.views.generic.base import TemplateView
 
 from registration.backends.simple.views import RegistrationView
 
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, request, user):
-        return "/upload/new"
-
 
 urlpatterns = patterns('',
-                       url(r'^register/$', MyRegistrationView.as_view(), name='registration_register'),
+                       url(r'^register/$',
+                           RegistrationView.as_view(),
+                           name='registration_register'),
                        url(r'^register/closed/$',
                            TemplateView.as_view(template_name='registration/registration_closed.html'),
                            name='registration_disallowed'),
